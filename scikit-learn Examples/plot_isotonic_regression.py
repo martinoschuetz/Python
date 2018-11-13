e6b8f8bbd88a6aaa44d1bcd07baab4332f19344c
Martin Schütz <martin@schuetzens.de>
2018-11-13 21:53:09 +0100

    Initial version

--- scikit-learn Examples/plot_isotonic_regression.py
+++ scikit-learn Examples/plot_isotonic_regression.py
@@ -1,3 +1,16 @@
+"""
+===================
+Isotonic Regression
+===================
+
+An illustration of the isotonic regression on generated data. The
+isotonic regression finds a non-decreasing approximation of a function
+while minimizing the mean squared error on the training data. The benefit
+of such a model is that it does not assume any form for the target
+function such as linearity. For comparison a linear regression is also
+presented.
+
+"""
 print(__doc__)
 
 # Author: Nelle Varoquaux <nelle.varoquaux@gmail.com>
